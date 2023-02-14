084d352800262356b6efa5c806af22d6d1b4095c
ZeGuilhermeYung <jgryung@gmail.com>
2023-02-14 16:34:32 -0300

    test 4

--- src/app.js
+++ src/app.js
@@ -7,6 +7,5 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, () => {
-    console.log("Servidor na porta " + 5000);
-});
+app.listen(process.env.PORT ,
+  ()=> console.log('Servidor na porta 5000.') );
