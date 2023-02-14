682e08ea94895f30f0915f22b93ed394de498895
ZeGuilhermeYung <jgryung@gmail.com>
2023-02-14 17:41:11 -0300

    test 9

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 import express from "express";
-import categoriesRouter from "./categories.js";
 import gamesRouter from "./games.js";
 import customersRouter from "./customers.js";
 import rentalsRouter from "./rentals.js";
 
 const router = express.Router();
-router.use(categoriesRouter);
 router.use(gamesRouter);
 router.use(customersRouter);
 router.use(rentalsRouter);
