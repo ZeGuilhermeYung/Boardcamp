cda393e9cfba19cd6aad9e070e16d471fb847486
ZeGuilhermeYung <jgryung@gmail.com>
2023-07-31 17:49:00 -0300

    deply

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, () => {
-  console.log("Servidor na porta " + 5000);
-});
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
