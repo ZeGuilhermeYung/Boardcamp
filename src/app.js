0beb90eb8022eb04ea47dbc1869ba6bf5a5c1219
ZeGuilhermeYung <jgryung@gmail.com>
2023-09-24 23:35:46 -0300

    reinstall node modules

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/index.js";
-import pg from "pg"
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
@@ -10,5 +9,5 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
