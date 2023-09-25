5a2c7b8f17981a2f7624ef0b73f05bb3c1bb76eb
ZeGuilhermeYung <jgryung@gmail.com>
2023-09-25 11:53:47 -0300

    applying string on date schema

--- src/schemas/customers.js
+++ src/schemas/customers.js
@@ -20,7 +20,7 @@ const customerSchema = joi.object({
         .pattern(/^\d+$/)
         .length(11)
         .required(),
-    birthday: joi.date()
+    birthday: joi.string()
         .pattern(/^(?:19|20)\d\d-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12][0-9]|3[01])$/)
         .required()
 });
