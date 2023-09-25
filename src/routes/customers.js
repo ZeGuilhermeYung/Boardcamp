139f898ab2f7627032ceca2476c2609df95d87b7
ZeGuilhermeYung <jgryung@gmail.com>
2023-09-25 14:42:40 -0300

    validation single search customers

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { createCustomer, readSingleCustomer, readCustomers, updateCustomer } from "../controllers/customers.js";
-import { customersSearchValidation, customerBodyValidation, uniqueCustomerValidation, customerIdValidation, customerIdSearchValidation, customerCPFValidation } from "../middlewares/customers.js";
+import { customersSearchValidation, customerBodyValidation, uniqueCustomerValidation, customerIdValidation, customerCPFValidation } from "../middlewares/customers.js";
 
 const customersRouter = express.Router();
 
 customersRouter.get("/customers", customersSearchValidation, readCustomers);
-customersRouter.get("/customers/:id", customerIdValidation, readSingleCustomer, customerIdSearchValidation);
+customersRouter.get("/customers/:id", customerIdValidation, readSingleCustomer);
 customersRouter.post("/customers", customerBodyValidation, uniqueCustomerValidation, createCustomer);
 customersRouter.put("/customers/:id", customerBodyValidation, customerIdValidation, customerCPFValidation, updateCustomer);
 
