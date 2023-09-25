f9e13fa75abc49d06fbdc800f65f30661795998f
ZeGuilhermeYung <jgryung@gmail.com>
2023-09-25 01:03:21 -0300

    correcting routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import categoriesRouter from "./categories.js";
 import gamesRouter from "./games.js";
 import customersRouter from "./customers.js";
 import rentalsRouter from "./rentals.js";
 
 const router = express.Router();
+router.use(categoriesRouter);
 router.use(gamesRouter);
 router.use(customersRouter);
 router.use(rentalsRouter);
