b323e9335d69615885061e431fe9bd5a63f6ec68
ZeGuilhermeYung <jgryung@gmail.com>
2023-09-25 11:51:01 -0300

    correcting birthday date schema

--- src/schemas/customers.js
+++ src/schemas/customers.js
@@ -21,6 +21,7 @@ const customerSchema = joi.object({
         .length(11)
         .required(),
     birthday: joi.date()
+        .pattern(/^(?:19|20)\d\d-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12][0-9]|3[01])$/)
         .required()
 });
 
